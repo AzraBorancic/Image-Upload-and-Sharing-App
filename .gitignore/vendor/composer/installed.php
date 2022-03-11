742d883fc2d4c508e136cc54251d4368fa1a4512
Azra Muhibic <azra.borancic@stu.ibu.edu.ba>
2022-03-11 11:40:49 +0100

    First commit

---
+++ .gitignore/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4f3c6b4e6787fc213c8826b240b4a562b97bdaca',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4f3c6b4e6787fc213c8826b240b4a562b97bdaca',
+            'dev_requirement' => false,
+        ),
+    ),
+);
