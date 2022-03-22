b7bc4efc66df5b5e5a2f2ae832a840f0990a9822
Azra Muhibic <azra.borancic@stu.ibu.edu.ba>
2022-03-22 14:35:32 +0100

    Restful endpoint to get all users

---
+++ rest/index.php
@@ -0,0 +1,48 @@
+<?php
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+require_once 'dao/ImageSharingDao.class.php';
+require_once '../vendor/autoload.php';
+
+/**
+ * List all users
+ */
+Flight::route('GET /users',function(){
+    $dao = new ImageSharingDao();
+    $users = $dao->get_all();
+    Flight::json($users);
+});
+
+/**
+ * List individual user
+ */
+
+/**
+ * Add user
+ */
+
+/**
+ * Update user
+ */
+
+/**
+ * Delete user
+ */
+Flight::route('/',function(){
+    echo 'Welcome to the web page for Image sharing and uploading app!';
+});
+
+Flight::route('/azra',function(){
+    echo 'Hello Azra';
+});
+
+Flight::route('/haris/@name',function($name){
+    echo 'Hello Haris!'. $name;
+});
+
+Flight::start();
+
+?>
