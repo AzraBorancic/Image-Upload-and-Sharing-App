bb5fece6f5321cb252bc9435b0a755ea5270b4cc
Azra Muhibic <azra.borancic@stu.ibu.edu.ba>
2022-04-10 18:06:00 +0200

    implemented image upload to s3

--- js/image-upload.js
+++ js/image-upload.js
@@ -1,54 +1,85 @@
 // Code By Webdevtrick ( https://webdevtrick.com )
 function readFile(input) {
-    if (input.files && input.files[0]) {
-      for (const file of input.files) {
-        var reader = new FileReader();
-        console.log(file)
-        reader.onload = function(e) {
-            var htmlPreview =
-              '<img width="200" src="' + e.target.result + '" />' +
-              '<p>' + file.name + '</p>';
-            var wrapperZone = $(input).parent();
-            var previewZone = $(input).parent().parent().find('.preview-zone');
-            var boxZone = $(input).parent().parent().find('.preview-zone').find('.box').find('.box-body');
-      
-            // wrapperZone.removeClass('dragover');
-            // previewZone.removeClass('hidden');
-            // boxZone.empty();
-            boxZone.append(htmlPreview);
-        };
+  if (input.files && input.files[0]) {
+    var wrapperZone = $(input).parent();
+    var previewZone = $(input).parent().parent().find('.preview-zone');
+    var boxZone = $(input).parent().parent().find('.preview-zone').find('.box').find('.box-body');
 
-        reader.readAsDataURL(file);
-      }
+    wrapperZone.removeClass('dragover');
+    previewZone.removeClass('hidden');
+    boxZone.empty();
+    for (const file of input.files) {
+      var reader = new FileReader();
+      console.log(file)
+      reader.onload = function (e) {
+        var htmlPreview =
+          '<img width="200" src="' + e.target.result + '" />' +
+          '<p>' + file.name + '</p>';
+        boxZone.append(htmlPreview);
+      };
+
+      reader.readAsDataURL(file);
     }
   }
-  
-  function reset(e) {
-    e.wrap('<form>').closest('form').get(0).reset();
-    e.unwrap();
+}
+
+function uploadFile() {
+  var fd = new FormData();
+  var files = $('.dropzone')[0].files;
+
+  if (files && files[0]) {
+
+    for (const file of files) {
+      fd.append(file.name, file);
+    }
+
+    $.ajax({
+      url: 'rest/images',
+      type: 'post',
+      data: fd,
+      contentType: false,
+      processData: false,
+      success: function (response) {
+        if (response != 0) {
+          console.log(response)
+        } else {
+          console.log('file not uploaded');
+        }
+      },
+    });
   }
-  
-  $(".dropzone").change(function() {
-    readFile(this);
-  });
-  
-  $('.dropzone-wrapper').on('dragover', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).addClass('dragover');
-  });
-  
-  $('.dropzone-wrapper').on('dragleave', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).removeClass('dragover');
-  });
-  
-  $('.remove-preview').on('click', function() {
-    var boxZone = $(this).parents('.preview-zone').find('.box-body');
-    var previewZone = $(this).parents('.preview-zone');
-    var dropzone = $(this).parents('.form-group').find('.dropzone');
-    boxZone.empty();
-    previewZone.addClass('hidden');
-    reset(dropzone);
-  });
+}
+
+function reset(e) {
+  e.wrap('<form>').closest('form').get(0).reset();
+  e.unwrap();
+}
+
+$("#upload-button").click(function () {
+  uploadFile();
+});
+
+$(".dropzone").change(function () {
+  readFile(this);
+});
+
+$('.dropzone-wrapper').on('dragover', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(this).addClass('dragover');
+});
+
+$('.dropzone-wrapper').on('dragleave', function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(this).removeClass('dragover');
+});
+
+$('.remove-preview').on('click', function () {
+  var boxZone = $(this).parents('.preview-zone').find('.box-body');
+  var previewZone = $(this).parents('.preview-zone');
+  var dropzone = $(this).parents('.form-group').find('.dropzone');
+  boxZone.empty();
+  previewZone.addClass('hidden');
+  reset(dropzone);
+});
--- rest/index.php
+++ rest/index.php
@@ -1,44 +1,50 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'dao\ImageSharingDao.class.php';
 require_once '..\vendor\autoload.php';
 
+use Dotenv\Dotenv;
+use Aws\S3\S3Client;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
 Flight::register('imageSharingDao', 'ImageSharingDao');
 
-Flight::route('/', function(){
+Flight::route('/', function () {
     echo 'hello world!';
 });
 
 /**
  * List all users
  */
-Flight::route('GET /users',function(){
+Flight::route('GET /users', function () {
     Flight::json(Flight::imageSharingDao()->get_all());
 });
 
 /**
  * List individual user
  */
-Flight::route('GET /users/@id',function($id){
+Flight::route('GET /users/@id', function ($id) {
     Flight::json(Flight::imageSharingDao()->get_by_id($id));
-    
 });
 
 /**
  * Add user
  */
-Flight::route('POST /users',function(){
+Flight::route('POST /users', function () {
     Flight::json(Flight::imageSharingDao()->add(Flight::request()->data->getData()));
 });
 
 /**
  * Update user
  */
-Flight::route('PUT /users/@id',function($id){
+Flight::route('PUT /users/@id', function ($id) {
     $data = Flight::request()->data->getData();
     $data['id'] = $id;
     Flight::json(Flight::imageSharingDao()->update($data));
@@ -47,11 +53,41 @@ Flight::route('PUT /users/@id',function($id){
 /**
  * Delete user
  */
-Flight::route('DELETE /users/@id',function($id){
+Flight::route('DELETE /users/@id', function ($id) {
     $user = Flight::imageSharingDao()->delete($id);
     Flight::json(["message" => "Deleted."]);
 });
 
-Flight::start();
+Flight::route('POST /images', function () {
 
-?>
+    $credentials = new Aws\Credentials\Credentials($_ENV['AWS_ACCESS_KEY_ID'], $_ENV['AWS_SECRET_ACCESS_KEY']);
+    $s3Client = new S3Client([
+        'version' => 'latest',
+        'region'  => $_ENV['AWS_DEFAULT_REGION'],
+        'credentials' => $credentials
+    ]);
+
+    var_dump($_ENV);
+
+    $files = Flight::request()->files;
+
+    foreach ($files as $file) {
+        try {
+
+
+            $name = time() . $file['name'];
+            $result = $s3Client->putObject([
+            'Bucket' => $_ENV['AWS_BUCKET'],
+            'Key'    => $name,
+            'Body'   => fopen($file['tmp_name'], 'r'),
+            'ACL'    => 'public-read', // make file 'public'
+            ]);
+            echo "Image uploaded successfully. Image path is: ". $result->get('ObjectURL');
+        } catch (Aws\S3\Exception\S3Exception $e) {
+            echo "There was an error uploading the file.\n";
+            echo $e->getMessage();
+        }
+    }
+});
+
+Flight::start();
