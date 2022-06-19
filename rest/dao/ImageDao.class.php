74c8c640cf644c13c1801f4fb45a7627dc13b68b
Azra Muhibic <azra.borancic@stu.ibu.edu.ba>
2022-06-19 17:21:28 +0200

    changed image query to return number of likes along with the image

--- rest/dao/ImageDao.class.php
+++ rest/dao/ImageDao.class.php
@@ -14,13 +14,13 @@ class ImageDao extends BaseDao
     }
 
     public function get_all_images() {
-        $query = "SELECT * FROM images";
+        $query = "SELECT i.id, i.s3_url, i.user_id, i.created_at, COUNT(uli.image_id) as number_of_likes from images i left join users_liked_images uli ON uli.image_id = i.id group by i.id;";
         return $this->query($query, []);
     }
 
     public function get_images($user_id, $search = NULL)
     {
-        $query = "SELECT * FROM images WHERE user_id = :user_id";
+        $query = "SELECT i.id, i.s3_url, i.user_id, i.created_at, COUNT(uli.image_id) as number_of_likes from images i left join users_liked_images uli ON uli.image_id = i.id WHERE i.user_id = :user_id group by i.id;";
         return $this->query($query, ['user_id' => $user_id]);
     }
 
