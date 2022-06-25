1f898ef8e6b44c54ac345268ecb7c0c09439d70c
Azra Muhibic <azra.borancic@stu.ibu.edu.ba>
2022-06-25 21:00:48 +0200

    added CORS support to API

--- rest/index.php
+++ rest/index.php
@@ -81,4 +81,18 @@ require_once __DIR__.'/routes/UserRoutes.php';
 require_once __DIR__.'/routes/ImageRoutes.php';
 require_once __DIR__.'/routes/AlbumRoutes.php';
 
+/**
+ * Set required headers and handle unwanted method types.
+ */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: authorization, Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
+header('Access-Control-Expose-Headers: Content-Range');
+/* Handle the OPTIONS request */
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    /* A 200 OK response to preflight with empty body */
+    header('HTTP/1.0 200');
+    die();
+}
+
 Flight::start();
